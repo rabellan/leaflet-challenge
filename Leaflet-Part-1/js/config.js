56a8de24177197752b7c4536a289f36082683926
rabellan <roland.abellano@gmail.com>
2023-12-19 16:55:48 -0800

    2nd commit with changes in logic1.js, logic2.js, and style.css

---
+++ Leaflet-Part-1/js/config.js
@@ -0,0 +1,3 @@
+// Put your API key here for testing
+// Put this in an environment variable if moving to real prod
+const api_key = "pk.eyJ1IjoicmFiZWxsYW4iLCJhIjoiY2xxY3lsbGkzMDNlMjJrbW9rY3FwajhoOCJ9.L2T0Am71fGxGZAQmoaewUw";
